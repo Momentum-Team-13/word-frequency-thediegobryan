9da319942050a0254f47f22b802405344d242c4c
Diego Bryan <thediegobryan@gmail.com>
2022-06-15 20:28:47 -0400

    text is filtered now

--- word_frequency.py
+++ word_frequency.py
@@ -1,13 +1,36 @@
+import string
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
 
+word_count = {}
+
+def filter_text(text):
+    remove_punctuation_text = text.translate(str.maketrans('', '', string.punctuation))
+    remove_uppercase_text = remove_punctuation_text.lower()
+    list_text = remove_uppercase_text.split()
+    for stop_word in STOP_WORDS:
+        for word in list_text:
+            if stop_word == word:
+                list_text.remove(word)
+    return list_text
+
+# def count_words(text):
+#     for word in text:
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    with open(file) as open_file:
+        read_file = open_file.read()
+        text_file = read_file
+    # print(text_file)
+
+    filtered_text = filter_text(text_file)
+    print(filtered_text)
+    # count_words(filtered_text)
 
 
 if __name__ == "__main__":
